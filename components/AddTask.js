bb10397919f4a97245f98a8a3450a2f286c53542
marvin espira <marvinespira@adalabsafrica.com>
2022-05-26 19:15:44 +0300

    feat: addtask update

--- components/AddTask.js
+++ components/AddTask.js
@@ -1,9 +1,16 @@
 const AddTask = () => {
     return ( 
         <div className="max-w-md px-4 pt-10 mx-auto sm:px-7 md:max-w-4xl md:px-6">
-            Add Task
+        <p className="font-bold">Add Task</p>
             <div className="pl-4 space-y-4 column">
-               Task: <input className="w-1/4 h-8"  type="text"></input>
+            <div className="mb-4">
+      <label className="block mb-2 text-sm font-bold text-gray-700">
+        Task Title
+      </label>
+      <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Task Title">
+         </input>
+    </div>
+               Task: <input className="w-1/4 h-8 border-black"  type="text"></input>
             <br />
                Time: <input className="w-1/4 h-10" type="data"></input>
             </div>
