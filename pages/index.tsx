747dfefa4abf3f75188009b981d2378c366657e3
marvin espira <espiramarvin@gmail.com>
2022-09-03 20:55:22 +0300

    fix: deletes add tasks

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,11 @@ import type { NextPage } from 'next'
 // import Head from 'next/head'
 // import Image from 'next/image'
 import Calendar from '../components/Calendar'
-import AddTask from '../components/AddTask'
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Calendar />
-      <AddTask />
     </div>
   )
 }
