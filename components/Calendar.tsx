10f88d64890bce9947b196ce13ac15aaa59ebc02
marvin espira <espiramarvin@gmail.com>
2022-09-04 13:41:57 +0300

    feat add updates

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -21,51 +21,8 @@ import MeetingsList from './MeetingsList'
     return classes.filter(Boolean).join(' ')
   }
 
-  const meetings = [
-    {
-      id: 1,
-      name: 'Leslie Alexander',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2022-05-11T13:00',
-      endDatetime: '2022-05-11T14:30',
-    },
-    {
-      id: 2,
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2022-05-20T09:00',
-      endDatetime: '2022-05-20T11:30',
-    },
-    {
-      id: 3,
-      name: 'Dries Vincent',
-      imageUrl:
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2022-05-20T17:00',
-      endDatetime: '2022-05-20T18:30',
-    },
-    {
-      id: 4,
-      name: 'Leslie Alexander',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2022-06-09T13:00',
-      endDatetime: '2022-06-09T14:30',
-    },
-    {
-      id: 5,
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      startDatetime: '2022-05-13T14:00',
-      endDatetime: '2022-05-13T14:30',
-    }
-  ]
-
   
-const Calendar = () => {
+const Calendar = ({ meetings }: any) => {
   
  let today = startOfToday()
   let [selectedDay, setSelectedDay] = useState(today)
@@ -166,7 +123,7 @@ const Calendar = () => {
                     </button>
     
                     <div className="w-1 h-1 mx-auto mt-1">
-                      {meetings.some((meeting) =>
+                      {meetings.some((meeting: any) =>
                         isSameDay(parseISO(meeting.startDatetime), day)
                       ) && (
                         <div className="w-1 h-1 rounded-full bg-sky-500"></div>
