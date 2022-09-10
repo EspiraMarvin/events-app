76ee56e17c5833c606dce6cfe791d614790a0130
marvin espira <espiramarvin@gmail.com>
2022-09-10 10:28:33 +0300

    feat: updates

---
+++ components/DatePicker.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { CalendarIcon } from "@heroicons/react/outline"
+
+interface DatePickerProps {
+    showFullCalendar: boolean
+    toggleShowCalendar: () => void
+}
+
+export default function DatePicker({ showFullCalendar, toggleShowCalendar }: DatePickerProps) {
+  return (
+    <div className="flex md:hidden">
+        <button
+            className='flex items-center justify-center w-full max-w-xs mx-auto border-2 shadow-xl h-14 gap-x-2'
+            onClick={toggleShowCalendar}
+        >
+            <CalendarIcon className='w-6 h-6' />
+            {showFullCalendar ? 'Hide Calendar' : 'Show Calendar'}
+        </button>
+    </div>
+  )
+}
