911b5dc1763cca47f45a11c2fd30320ae1727564
marvin espira <espiramarvin@gmail.com>
2022-09-12 13:39:11 +0300

    feat: add updates

--- components/ThemeSetting.tsx
+++ components/ThemeSetting.tsx
@@ -7,11 +7,11 @@ export default function ThemeSetting() {
   const { theme, setTheme } =  useContext<any | null>(ThemeContext)
 
   return (
-    <div className="flex justify-end pr-4 cursor-pointer md:pr-0 dark:text-white">
+    <div className="flex justify-end pr-4 md:pr-0 dark:text-white">
 
-        {theme === "dark" && <MoonIcon className='w-6 h-6 -mr-0.5' onClick={() => setTheme('light')} />  }
+        {theme === "dark" && <MoonIcon className='w-6 h-6 -mr-0.5 cursor-pointer' onClick={() => setTheme('light')} />  }
         
-        {theme === "light" && <SunIcon className='w-6 h-6' onClick={() => setTheme('dark')}  /> }   
+        {theme === "light" && <SunIcon className='w-6 h-6 cursor-pointer' onClick={() => setTheme('dark')}  /> }   
 
     </div>
   )
